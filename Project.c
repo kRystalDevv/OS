e81733b7b9947d6bb69044fb330e6f8af2546255
talha <74349172+kRystalDevv@users.noreply.github.com>
2023-01-24 19:12:27 +0500

    Added more stuff...

--- Project.c
+++ Project.c
@@ -20,7 +20,7 @@ int taxcalc(int price, int quantity)
 void options()
 {
     printf("1 -> Food Menu\n");
-    printf("2 -> Order and Price\n");
+    printf("2 -> Order and Price *UNDER MAINTAINENCE!*\n");
     printf("3 -> Exit to Menu\n");
 }
 
@@ -30,6 +30,20 @@ void foodmenu()
     printf("550 PKR -> McSpicy\n");
     printf("450 PKR -> McNotSpicy\n");
     printf("600 PKR -> McRoyale\n");
+}
+
+int orderHandler(int quantity, int itemtype)
+{
+    int i = 0, receipt = 0;
+    int foodtype[4] = {500, 550, 450, 600};
+    for(i = 0; i < 5; i++)
+    {
+        if(quantity != foodtype[i])
+        {
+            printf("Invalid Order!");
+            break;
+        }
+    }
 }
 
 void transaction()
@@ -52,13 +66,16 @@ void menu()
         printf("________________\n");
 		printf("Here's The Menu!\n\n");
         foodmenu();
+        order();
         break;
         case 2:
         printf("Showing your Order!\n");
         transaction();
         break;
         case 3:
+        system("cls");
         printf("Exiting back to Menu!\n");
+        menu();
         break;
         default:
         system("cls");
@@ -71,27 +88,38 @@ void order()
 {
     printf("\nDo you want to continue with your order?\n");
     int exit = 9;
-    printf("Enter %d finish ordering", exit);
+    printf("Enter %d finish ordering\nFood: ", exit);
     int choice = 0;
     int quantity = 1;
     int McChickenCounter = 0, McNotSpicy = 0, McRoyaleCounter = 0, McSpicyCounter = 0; 
     scanf("%d", &choice);
-    while(choice != exit)
-    {
-        switch(choice)
-        {
-            case 1:
-            printf("How many McChickens would you like?\nQuantity: ");
-            scanf("%d", &quantity);
-            McChickenCounter = quantity;
-            choice = 0;   
-        }
+	switch(choice)
+	    {
+	        case 1:
+	            printf("How many McChickens would you like?\nQuantity: ");
+	            scanf("%d", &quantity);
+	            McChickenCounter = quantity;
+	            choice = 0;
+	            
+	            break;
+	        case 2:
+	        	printf("How many McNotSpicy would you like?\nQuantity");
+	        	scanf("%d", &quantity);
+	        	McNotSpicy = quantity;
+	        	choice = 0;
+	        	break;
+	        case 9:
+	        	printf("Sucessfully Saved Your Order!");
+	        	printf("Would you like to proceed to Transaction?");
+	        	printf("Press 'Y' to Proceed, Press 'N' to Add More\n");
+	        	if(scanf("%c") != 'Y')
+	        	break;
     }
     
 }
 
 int main()
 {
+    int *fhandlerpointer;
     menu();
-    order();
 }
